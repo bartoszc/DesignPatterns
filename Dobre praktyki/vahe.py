fa5c0d3bc9369ec737a6f125d825cf88313172b6
Bartosz <bartosz.chojnacki@dsstream.com>
2024-12-18 10:22:36 +0100

    minor changes in a structure

--- Dobre praktyki/vahe.py
+++
@@ -1,6 +0,0 @@
-from functools import reduce
-
-
-def amount_payment(payment):
-    # return reduce(lambda x, y: x + y if y > 0 else x, payment, 0)
-    return reduce(lambda x, y: x + y if y > 0, payment, 0)
